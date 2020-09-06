1e59f286c683989c64c22b45e638107596bdbf00
Dure Sameen <duresameenod@hotmail.com>
2020-09-06 23:15:22 +0500

    Add project files.

---
+++ SampleApi/Controllers/CandidateController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleApi.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private static List<Candidate> list = new List<Candidate>();
+        // Get list of values
+        // GET: api/<CandidateController>
+        [HttpGet]
+        public IEnumerable<Candidate> Get()
+        {
+            return list;
+        }
+        //Get single record
+        // GET api/<CandidateController>/5
+        [HttpGet("{id}")]
+        public Candidate Get(int id)
+        {
+            return list.Where(record => record.CandidateId == id).FirstOrDefault();
+        }
+        //Create
+        // POST api/<CandidateController>
+        [HttpPost]
+        public void Post([FromBody] Candidate candidate)
+        {
+
+            list.Add(candidate);
+        }
+        //Edit
+        // PUT api/<CandidateController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Candidate candidate)
+        {
+           var candidateById= list.Where(record => record.CandidateId == id).FirstOrDefault();
+
+            candidateById.Email = candidate.Email;
+            candidateById.FirstName = candidate.FirstName;
+            candidateById.LastName = candidate.LastName;
+
+
+        }
+        //Delete
+        // DELETE api/<CandidateController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var candidateById = list.Where(record => record.CandidateId == id).FirstOrDefault();
+            list.Remove(candidateById);
+        }
+    }
+}
---
+++ SampleApi/Data/Candidate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SampleApi.Data
+{
+    public class Candidate
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string Email { get; set; }
+        public int  CandidateId { get; set; }
+        public Candidate()
+        { }
+        public Candidate(string firstName, string lastName, int candidateId, string email)
+        {   
+            FirstName = firstName;
+            LastName = lastName;
+            CandidateId = candidateId;
+            Email = email; 
+        }
+        
+         
+    }
+}
